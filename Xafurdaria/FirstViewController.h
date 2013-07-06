c19f5ee26114422df12899e4be6d16650ada939b
Iuri <iuri.to.sm@gmaill.com>
2013-07-05 22:39:22 -0300

    Initial Commit

---
+++ Xafurdaria/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Xafurdaria
+//
+//  Created by Iuri on 05/07/13.
+//  Copyright (c) 2013 Iuri Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Xafurdaria/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  Xafurdaria
+//
+//  Created by Iuri on 05/07/13.
+//  Copyright (c) 2013 Iuri Mac. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
