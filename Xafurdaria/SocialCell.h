e61d040267f7bd10d96afc2826adb04b07a3844c
Iuri Matsuura <iuri@tinas-mbp.lan>
2014-08-16 12:13:25 +0200

    Added apprater

---
+++ Xafurdaria/SocialCell.h
@@ -0,0 +1,15 @@
+//
+//  SocialCell.h
+//  4FitClub Tv
+//
+//  Created by Iuri Matsuura on 27/07/14.
+//  Copyright (c) 2014 Woerk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *socialImageLogo;
+
+@property (weak, nonatomic) IBOutlet UILabel *profileName;
+@end
---
+++ Xafurdaria/SocialCell.m
@@ -0,0 +1,34 @@
+//
+//  SocialCell.m
+//  4FitClub Tv
+//
+//  Created by Iuri Matsuura on 27/07/14.
+//  Copyright (c) 2014 Woerk. All rights reserved.
+//
+
+#import "SocialCell.h"
+
+@implementation SocialCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
